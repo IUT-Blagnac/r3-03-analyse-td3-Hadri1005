064336a908e56bcb2f54a53ab94c10bb0e3585e5
Hadri <hadrienbenoit1@gmail.com>
2025-09-15 13:13:46 +0200

    commentaire

--- trace/src/main/java/com/iutblagnac/r303trace/App.java
+++ trace/src/main/java/com/iutblagnac/r303trace/App.java
@@ -9,6 +9,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(hello());
     }
+    //Création de la méthode Hello World
 
     public static String hello() {
         return "Hello World!";
